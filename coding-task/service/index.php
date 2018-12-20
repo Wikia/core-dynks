2a0032f583bba2a3696c76aa256b49ac24400623
macbre <maciej.brencz@gmail.com>
2018-12-20 09:41:57 +0100

    Update README + call inject_failure()

--- coding-task/service/index.php
+++ coding-task/service/index.php
@@ -6,11 +6,24 @@ define('VERSION', '0.1');
 define('TOKEN', '0d5f153fb24410ec8c83092b976ac8205cd4a302');
 define('MESSAGE_FILE', '/tmp/.message');
 
+/**
+ * Validates provided X-Token request header
+ */
 function assert_token_is_valid() {
 	$token = $_SERVER['HTTP_X_TOKEN'];
 
 	if ($token !== TOKEN) {
 		halt( HTTP_UNAUTHORIZED, "Provided X-Token request header is not valid." );
+	}
+}
+
+/**
+ * We want this service to be a bit unreliable.
+ * Let's inject a failure (i.e. HTTP 502 response) for ~40% of responses
+ */
+function inject_failure() {
+	if ( mt_rand() % 100 < 40 ) {
+		halt(HTTP_SERVICE_UNAVAILABLE, 'We\'re having some technical issues. Please try again.');
 	}
 }
 
@@ -34,6 +47,7 @@ dispatch('/', function ()  {
 // gets a message
 dispatch_get('/message', function() {
 	assert_token_is_valid();
+	inject_failure();
 
 	$message = trim(file_get_contents(MESSAGE_FILE));
 
@@ -49,6 +63,7 @@ dispatch_get('/message', function() {
 // updates a message
 dispatch_post('/message', function() {
 	assert_token_is_valid();
+	inject_failure();
 
 	$message = $_POST['message'];
 	$res = file_put_contents(MESSAGE_FILE, $message);
